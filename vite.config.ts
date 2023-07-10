f6966a9f3b6213a4e259d5a784f2b071744d5835
Nathanael Cassagnol <nathanaelcassagnol@gmail.com>
2023-07-09 21:56:45 -0400

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,34 @@
 import { defineConfig } from 'vite';
-
+import tsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react';
+import envCompatible from 'vite-plugin-env-compatible';
+
+type ViteConfigInput = {
+  mode: string;
+  command: string;
+};
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // Fix AWS Amplify issue where incorrect entry point is used
-  // Error: 'request' is not exported by __vite-browser-external, imported by node_modules/@aws-sdk/credential-provider-imds/dist/es/remoteProvider/httpRequest.js
-  // Github Issue: https://github.com/aws/aws-sdk-js/issues/3673
-  resolve: {
-    alias: {
-      "./runtimeConfig": "./runtimeConfig.browser",
+export default (args: ViteConfigInput) => {
+  const generateScopedName =
+    args.mode === 'production' ? '[hash:base64:6]' : '[local]_[hash:base64:6]';
+
+  return defineConfig({
+    plugins: [react(), tsconfigPaths(), envCompatible()],
+    build: {
+      outDir: 'build',
+      chunkSizeWarningLimit: 4000,
     },
-  },
-});
+    css: {
+      modules: {
+        localsConvention: 'camelCase',
+        generateScopedName,
+      },
+    },
+    resolve: {
+      alias: {
+        './runtimeConfig': './runtimeConfig.browser',
+      },
+    },
+  });
+};
