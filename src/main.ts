c33ad417e2ed45c69d98ea1a893864e141a785b9
Nathanael Cassagnol <nathanaelcassagnol@gmail.com>
2024-01-08 22:55:52 -0500

    build package.json errors

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/config/app.config';
 import { AppComponent } from './app/config/app.component';
+import { Amplify } from 'aws-amplify';
+import amplifyconfig from './amplifyconfiguration.json';
+Amplify.configure(amplifyconfig);
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
