89126c93b6c224324df5ed22e607c3653c13658e
Nathanael Cassagnol <nathanaelcassagnol@gmail.com>
2024-01-23 19:08:24 -0500

    Amplify setup

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,20 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "healthnow365": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "healthnow3651fe0c539": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/config/app.config';
 import { AppComponent } from './app/config/app.component';
+
 import { Amplify } from 'aws-amplify';
-import amplifyconfig from './amplifyconfiguration.json';
-Amplify.configure(amplifyconfig);
+//import amplifyconfig from './amplifyconfiguration.json';
+//Amplify.configure(amplifyconfig);
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
